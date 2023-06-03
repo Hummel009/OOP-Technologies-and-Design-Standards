06d36a20b3b27cec665b9789e2603013fdaf4ee9
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-06-03 16:16:13 +0300

    unneeded

--- Kornet/src/kornet/optional/Standard.kt
+++
@@ -1,8 +0,0 @@
-package kornet.optional
-
-interface Standard {
-	fun getFirstStandardValue(): String
-	fun setFirstStandardValue(value: String)
-	fun getSecondStandardValue(): String
-	fun setSecondStandardValue(value: String)
-}
--- Kornet/src/kornet/optional/Super.kt
+++
@@ -1,8 +0,0 @@
-package kornet.optional
-
-interface Super {
-	fun getFirstSuperValue(): String
-	fun setFirstSuperValue(value: String)
-	fun getSecondSuperValue(): String
-	fun setSecondSuperValue(value: String)
-}
