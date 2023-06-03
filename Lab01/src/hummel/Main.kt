bf830655c88bb75738b397f07b32ca895f21ac50
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-06-03 13:37:46 +0300

    Рефакторинг

--- Lab01/src/hummel/Main.kt
+++ Lab01/src/hummel/Main.kt
@@ -1,121 +1,118 @@
 package hummel
 
 import java.util.*
-import kotlin.collections.ArrayList
 
 val transport: MutableList<Transport> = ArrayList()
+val functions: MutableMap<String, () -> Unit> = HashMap()
 
 fun main() {
+	init()
 	val scan = Scanner(System.`in`)
 
 	loop@ while (true) {
-		println("Enter the function:")
-		when (scan.nextLine()) {
-			"price" -> {
-				searchByPrice()
-			}
+		println("Enter the command:")
 
-			"color" -> {
-				searchByColor()
-			}
+		val command = scan.nextLine()
+		functions[command]?.invoke()
 
-			"name" -> {
-				searchByName()
-			}
-
-			"clear" -> {
-				transport.clear()
-			}
-
-			"show" -> {
-				show()
-			}
-
-			"load" -> {
-				transport.addAll(loadDefaultList())
-			}
-
-			"exit" -> break@loop
+		if (command == "exit") {
+			break@loop
 		}
 	}
 }
 
-fun show() {
-	for (car in transport) {
-		println(car.getTheInfo())
+fun init() {
+	functions["commands"] = ::showAllCommands
+	functions["show"] = ::showAllTransport
+	functions["color"] = ::searchByColor
+	functions["price"] = ::searchByPrice
+	functions["name"] = ::searchByName
+	functions["clear"] = { transport.clear() }
+	functions["load"] = { transport.addAll(loadDefaultList()) }
+}
+
+fun showAllCommands() {
+	for (item in functions.keys) {
+		println(item)
+	}
+}
+
+fun showAllTransport() {
+	for (item in transport) {
+		println(item.getTheInfo())
 	}
 }
 
 fun searchByName() {
-	println("Enter the name of the transport")
+	println("Enter the name of the transport:")
 	val scan = Scanner(System.`in`)
-	val str = scan.nextLine()
+	val name = scan.nextLine()
 	var found = false
 
-	for (car in transport) {
-		if (car.getTheName() == str) {
-			println(car.getTheInfo())
+	for (item in transport) {
+		if (item.getTheName() == name) {
+			println(item.getTheInfo())
 			found = true
 		}
 	}
 
 	if (!found) {
-		println("No info found")
+		println("No items found!")
 	}
 }
 
 fun searchByColor() {
-	println("Enter the color of the transport")
+	println("Enter the color of the transport:")
 	val scan = Scanner(System.`in`)
-	val str = scan.nextLine()
+	val color = scan.nextLine()
 	var found = false
 
-	for (car in transport) {
-		if (car.getTheColor() == str) {
-			println(car.getTheInfo())
+	for (item in transport) {
+		if (item.getTheColor() == color) {
+			println(item.getTheInfo())
 			found = true
 		}
 	}
 
 	if (!found) {
-		println("No info found")
+		println("No items found!")
 	}
 }
 
 fun searchByPrice() {
-	println("Enter the price of the transport")
+	println("Enter the price of the transport:")
 	val scan = Scanner(System.`in`)
 	val price = scan.nextInt()
 	var found = false
 
-	for (car in transport) {
-		if (car.getThePrice() == price) {
-			println(car.getTheInfo())
+	for (item in transport) {
+		if (item.getThePrice() == price) {
+			println(item.getTheInfo())
 			found = true
 		}
 	}
 
 	if (!found) {
-		println("No info found")
+		println("No items found!")
 	}
 }
 
 fun loadDefaultList(): MutableList<Transport> {
-	val transports = ArrayList<Transport>()
-	transports.add(BicycleAist("Red", 150))
-	transports.add(BicycleStels("Red"))
-	transports.add(CarVolkswagen("Red", 18500))
-	transports.add(CarLada("Red"))
+	val transport = ArrayList<Transport>()
+	transport.add(BicycleAist("Red", 150))
+	transport.add(BicycleStels("Red"))
+	transport.add(CarVolkswagen("Red", 18500))
+	transport.add(CarLada("Red"))
 
-	transports.add(BicycleAist("Green"))
-	transports.add(BicycleStels("Green", 350))
-	transports.add(CarVolkswagen("Green"))
-	transports.add(CarLada("Green"))
+	transport.add(BicycleAist("Green"))
+	transport.add(BicycleStels("Green", 350))
+	transport.add(CarVolkswagen("Green"))
+	transport.add(CarLada("Green"))
 
-	transports.add(BicycleAist("Blue"))
-	transports.add(BicycleStels("Blue"))
-	transports.add(CarVolkswagen("Blue"))
-	transports.add(CarLada("Blue", 5500))
+	transport.add(BicycleAist("Blue"))
+	transport.add(BicycleStels("Blue"))
+	transport.add(CarVolkswagen("Blue"))
+	transport.add(CarLada("Blue", 5500))
 
-	return transports
+	return transport
 }
--- Lab02/src/hummel/Main.kt
+++ Lab02/src/hummel/Main.kt
@@ -3,139 +3,137 @@ package hummel
 import java.util.*
 
 val transport: MutableList<Transport> = ArrayList()
+val functions: MutableMap<String, () -> Unit> = HashMap()
 
 fun main() {
+	init()
 	val scan = Scanner(System.`in`)
 
 	loop@ while (true) {
-		println("Enter the function:")
-		when (scan.nextLine()) {
-			"price" -> {
-				searchByPrice()
-			}
+		println("Enter the command:")
 
-			"color" -> {
-				searchByColor()
-			}
+		val command = scan.nextLine()
+		functions[command]?.invoke()
 
-			"name" -> {
-				searchByName()
-			}
-
-			"clear" -> {
-				transport.clear()
-			}
-
-			"show" -> {
-				show()
-			}
-
-			"load" -> {
-				transport.addAll(loadDefaultList())
-			}
-
-			"sell" -> {
-				sell()
-			}
-
-			"exit" -> break@loop
+		if (command == "exit") {
+			break@loop
 		}
 	}
 }
 
-fun sell() {
-	println("Enter the name of the transport")
+fun init() {
+	functions["commands"] = ::showAllCommands
+	functions["show"] = ::showAllTransport
+	functions["color"] = ::searchByColor
+	functions["price"] = ::searchByPrice
+	functions["name"] = ::searchByName
+	functions["sell"] = ::addTransport
+	functions["clear"] = { transport.clear() }
+	functions["load"] = { transport.addAll(loadDefaultList()) }
+}
+
+fun showAllCommands() {
+	for (item in functions.keys) {
+		println(item)
+	}
+}
+
+fun showAllTransport() {
+	for (item in transport) {
+		println(item.getTheInfo())
+	}
+}
+
+fun addTransport() {
+	println("Enter the class name of the transport:")
 	val scan = Scanner(System.`in`)
 	val name = scan.nextLine()
 
-	val className = Class.forName("hummel.$name")
+	try {
+		val itemClass = Class.forName("hummel.$name")
+		println("Enter the price of the transport:")
+		val price = scan.nextLine().toInt()
+		println("Enter the color of the transport:")
+		val color = scan.nextLine()
 
-	println("Enter the price of the transport")
-	val price = scan.nextLine().toInt()
-	println("Enter the color of the transport")
-	val color = scan.nextLine()
+		val item = itemClass.getConstructor(Int::class.java, String::class.java).newInstance(price, color) as Transport
 
-	val obj = className.getConstructor(Int::class.java, String::class.java).newInstance(price, color) as Transport
-
-	transport.add(obj)
-}
-
-fun show() {
-	for (car in transport) {
-		println(car.getTheInfo())
+		transport.add(item)
+	} catch (e: Exception) {
+		println("Unknown class!")
 	}
 }
 
 fun searchByName() {
-	println("Enter the name of the transport")
+	println("Enter the name of the transport:")
 	val scan = Scanner(System.`in`)
-	val str = scan.nextLine()
+	val name = scan.nextLine()
 	var found = false
 
-	for (car in transport) {
-		if (car.getTheName() == str) {
-			println(car.getTheInfo())
+	for (item in transport) {
+		if (item.getTheName() == name) {
+			println(item.getTheInfo())
 			found = true
 		}
 	}
 
 	if (!found) {
-		println("No info found")
+		println("No items found!")
 	}
 }
 
 fun searchByColor() {
-	println("Enter the color of the transport")
+	println("Enter the color of the transport:")
 	val scan = Scanner(System.`in`)
-	val str = scan.nextLine()
+	val color = scan.nextLine()
 	var found = false
 
-	for (car in transport) {
-		if (car.getTheColor() == str) {
-			println(car.getTheInfo())
+	for (item in transport) {
+		if (item.getTheColor() == color) {
+			println(item.getTheInfo())
 			found = true
 		}
 	}
 
 	if (!found) {
-		println("No info found")
+		println("No items found!")
 	}
 }
 
 fun searchByPrice() {
-	println("Enter the price of the transport")
+	println("Enter the price of the transport:")
 	val scan = Scanner(System.`in`)
 	val price = scan.nextInt()
 	var found = false
 
-	for (car in transport) {
-		if (car.getThePrice() == price) {
-			println(car.getTheInfo())
+	for (item in transport) {
+		if (item.getThePrice() == price) {
+			println(item.getTheInfo())
 			found = true
 		}
 	}
 
 	if (!found) {
-		println("No info found")
+		println("No items found!")
 	}
 }
 
 fun loadDefaultList(): MutableList<Transport> {
-	val transports = ArrayList<Transport>()
-	transports.add(BicycleAist(color = "Red"))
-	transports.add(BicycleStels(color = "Red"))
-	transports.add(CarVolkswagen(color = "Red"))
-	transports.add(CarLada(color = "Red"))
+	val transport = ArrayList<Transport>()
+	transport.add(BicycleAist(color = "Red"))
+	transport.add(BicycleStels(color = "Red"))
+	transport.add(CarVolkswagen(color = "Red"))
+	transport.add(CarLada(color = "Red"))
 
-	transports.add(BicycleAist(250, "Green"))
-	transports.add(BicycleStels(350, "Green"))
-	transports.add(CarVolkswagen(18500, "Green"))
-	transports.add(CarLada(6500, "Green"))
+	transport.add(BicycleAist(250, "Green"))
+	transport.add(BicycleStels(350, "Green"))
+	transport.add(CarVolkswagen(18500, "Green"))
+	transport.add(CarLada(6500, "Green"))
 
-	transports.add(BicycleAist(150, "Blue"))
-	transports.add(BicycleStels(250, "Blue"))
-	transports.add(CarVolkswagen(17500, "Blue"))
-	transports.add(CarLada(5500, "Blue"))
+	transport.add(BicycleAist(150, "Blue"))
+	transport.add(BicycleStels(250, "Blue"))
+	transport.add(CarVolkswagen(17500, "Blue"))
+	transport.add(CarLada(5500, "Blue"))
 
-	return transports
+	return transport
 }
