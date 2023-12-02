b85bea36558d7967a223aaea72f4580a9ec8fd82
Hummel009 <hummel.turbamentum@gmail.com>
2023-12-02 18:00:11 +0300

    Java 21, Kotlin 1.9.21, Gradle 8.5

--- Lab03/build.gradle.kts
+++ Lab03/build.gradle.kts
@@ -2,7 +2,7 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 plugins {
-	id("org.jetbrains.kotlin.jvm") version "1.9.20"
+	id("org.jetbrains.kotlin.jvm") version "1.9.21"
 	id("application")
 }
 
@@ -16,7 +16,7 @@ repositories {
 val embed: Configuration by configurations.creating
 
 dependencies {
-	embed("org.jetbrains.kotlin:kotlin-stdlib:1.9.20")
+	embed("org.jetbrains.kotlin:kotlin-stdlib:1.9.21")
 	embed("com.google.code.gson:gson:2.10.1")
 	embed("javax.xml.bind:jaxb-api:2.2.4")
 	implementation("com.google.code.gson:gson:2.10.1")
@@ -25,7 +25,7 @@ dependencies {
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
+		languageVersion = JavaLanguageVersion.of(21)
 	}
 }
 
