78df3e4aea3effae33107910c16815dedcc87ddb
Hummel009 <hummel.turbamentum@gmail.com>
2024-05-12 15:47:34 +0300

    Kotlin 1.9.24

--- appLab6/build.gradle.kts
+++ appLab6/build.gradle.kts
@@ -12,7 +12,7 @@ version = LocalDate.now().format(DateTimeFormatter.ofPattern("yy.MM.dd"))
 val embed: Configuration by configurations.creating
 
 dependencies {
-	embed("org.jetbrains.kotlin:kotlin-stdlib:1.9.23")
+	embed("org.jetbrains.kotlin:kotlin-stdlib:1.9.24")
 	embed("com.google.code.gson:gson:2.10.1")
 	implementation("com.google.code.gson:gson:2.10.1")
 }
